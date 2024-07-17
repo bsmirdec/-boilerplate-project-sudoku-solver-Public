ccf43e952f07fa7cca932a87755a6ea9a0968989
Bogdan SMIRDEC <bogdan.smirdec@gmail.com>
2024-07-17 04:14:58 +0200

    First commit

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,24 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (puzzleString.length !== 81) {
+      return false;
+    }
+    const chars = puzzleString.split("");
+    for (let char of chars) {
+      if (isNaN(char) && char !== ".") {
+        return false;
+      }
+    }
+    return true;
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,22 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const SudokuSolver = require("../controllers/sudoku-solver.js");
+let solver = new SudokuSolver();
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  const validString =
+    "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+  const invalidString =
+    "1.5..2.84..63.12.7.2..5...?.9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+  const shortString = "13431...124...";
+  test("81 valid characters string should be true", () => {
+    assert.isTrue(solver.validate(validString));
+  });
+  test("81 invalid characters string should be falsy", () => {
+    assert.isFalse(solver.validate(invalidString));
+  });
+  test("short valid characters string should be falsy", () => {
+    assert.isFalse(solver.validate(shortString));
+  });
 });
